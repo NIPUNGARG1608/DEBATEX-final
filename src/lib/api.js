cdbd2007fe22c0c55593ae5c5d8bdd6c9fde1f32
NIPUNGARG1608 <gargnipun1606@gmail.com>
2026-07-08 17:33:27 +0530

    Add withCredentials to axios for CORS compatibility

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,10 @@ import axios from "axios";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API_BASE = `${BACKEND_URL}/api`;
 
-const api = axios.create({ baseURL: API_BASE });
+const api = axios.create({ 
+  baseURL: API_BASE,
+  withCredentials: true,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("debatex_token");
